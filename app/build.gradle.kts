fd8eb762189a8d970dd37595f89b8e8fe6aa8992
wtao <wtao@redfast.com>
2025-08-08 10:28:18 -0700

    change miniSDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.redfast.mpass"
-        minSdk = 21
+        minSdk = 25
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
