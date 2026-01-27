57077ddf0d7c8780d73d8e18ed2c3c95ca95cf37
Pía Villalba <pvillalbamoreno@recurly.com>
2026-01-27 15:30:06 -0500

    [RE-561] feat: sync redfast-sdk-android-build with redfast-sdk-android

--- app/src/main/java/com/redfast/mpass/redflix/genres/adapter/holders/BillboardHolder.kt
+++ app/src/main/java/com/redfast/mpass/redflix/genres/adapter/holders/BillboardHolder.kt
@@ -8,25 +8,27 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.redfast.mpass.MovieFragment
 import com.redfast.mpass.R
+import com.redfast.promotion.IapProductType
 import com.redfast.promotion.PromotionManager
 
 class BillboardHolder(v: View) : RecyclerView.ViewHolder(v) {
     init {
         itemView.setOnClickListener {
-            PromotionManager.getIapItems("com.redfast.test.consumable") { list ->
-                if (list.isNotEmpty()) {
+            PromotionManager.iapGetProductDetails("com.redfast.test.consumable", IapProductType.consumable) { products ->
+                if (products.isNotEmpty()) {
                     val builder = AlertDialog.Builder(itemView.context)
                     builder.setTitle("InApp Purchase")
-                    builder.setMessage("Do you want to purchase " + list[0].title + " for " + list[0].price)
+                    builder.setMessage("Do you want to purchase " + products[0].title + " for " + products[0].price)
                     builder.setPositiveButton("YES") { _, _ ->
-                        PromotionManager.purchaseIap(list[0].sku!!) {
-                            val fragmentManager =
-                                (v.context as AppCompatActivity).supportFragmentManager
-                            val fragmentTransaction = fragmentManager.beginTransaction()
-                            val fragment = MovieFragment()
-                            fragmentTransaction.add(R.id.container, fragment, "Movie")
-                            fragmentTransaction.addToBackStack("Home")
-                            fragmentTransaction.commit()
+                        PromotionManager.iapPurchaseProducts(listOf(products[0].rawObj), null) { purchases, error ->
+                            if (error == null && purchases.isNotEmpty()) {
+                                val fragmentManager = (v.context as AppCompatActivity).supportFragmentManager
+                                val fragmentTransaction = fragmentManager.beginTransaction()
+                                val fragment = MovieFragment()
+                                fragmentTransaction.add(R.id.container, fragment, "Movie")
+                                fragmentTransaction.addToBackStack("Home")
+                                fragmentTransaction.commit()
+                            }
                         }
                     }
                     builder.create().show()
